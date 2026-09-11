5bfe5fd1da5a2d05f4742a95b17a9e75cfa48b5e
megasharkGit <megashark@gmail.com>
2026-09-11 15:11:49 +0100

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,13 +1,8 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
-
-# O nome do módulo TEM de coincidir com o .so esperado pela app:
-#   libAndroidGame001JNI.so  ->  LOCAL_MODULE := AndroidGame001JNI
-LOCAL_MODULE    := AndroidGame001JNI
+LOCAL_MODULE    := AndroidGame001JNI            # -> libAndroidGame001JNI.so
 LOCAL_SRC_FILES := tama_engine.c
-
-LOCAL_LDLIBS    := -llog -lGLESv1_CM -lm
-LOCAL_CFLAGS    := -O2 -Wall -Wextra
-
+LOCAL_LDLIBS    := -llog -lGLESv1_CM -lEGL -lm
+LOCAL_CFLAGS    := -O2 -Wall -Wextra -fvisibility=hidden \
+                   -DJNIEXPORT='__attribute__((visibility("default")))'
 include $(BUILD_SHARED_LIBRARY)
